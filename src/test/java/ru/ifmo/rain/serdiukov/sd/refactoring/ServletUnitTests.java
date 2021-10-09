bb2c55b794065de5e1bc2abbc803fd9178777cf7
Alexander <ntwwwnt@gmail.com>
2021-10-09 20:34:51 +0300

    Added an HTTP Client and implemented product-get method

--- src/test/java/ru/ifmo/rain/serdiukov/sd/refactoring/ServletUnitTests.java
+++ src/test/java/ru/ifmo/rain/serdiukov/sd/refactoring/ServletUnitTests.java
@@ -1,12 +1,52 @@
 package ru.ifmo.rain.serdiukov.sd.refactoring;
 
-import org.eclipse.jetty.client.HttpClient;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import ru.akirakozov.sd.refactoring.Main;
+import ru.ifmo.rain.serdiukov.sd.refactoring.util.APIRequestException;
+import ru.ifmo.rain.serdiukov.sd.refactoring.util.AppHTTPClient;
+import ru.ifmo.rain.serdiukov.sd.refactoring.util.Product;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.fail;
 
 public class ServletUnitTests {
+    private AppHTTPClient appClient;
+    private ExecutorService exService;
+
+    @Before
+    public void startServerAndAttachClient() throws Exception {
+        exService = Executors.newSingleThreadExecutor();
+        exService.execute(new Runnable() { // or use submit to get a Future (a result of computation, you'll need a Callable, rather than runnable then)
+            @Override
+            public void run() {
+                try {
+                    Main.main(new String[0]);
+                } catch (final Exception e) {
+                    throw new RuntimeException("Server thread has encountered an exception", e);
+                }
+            }
+        });
+        exService.shutdown();
+        appClient = new AppHTTPClient("http://localhost:8081");
+    }
 
     @Test
-    public void someTest(){
-        // Added to check compilation warnings
+    public void someTest() {
+        try {
+            final List<Product> products = appClient.getProducts();
+            products.size();
+        } catch (APIRequestException e) {
+            fail("No exception was expected");
+        }
+    }
+
+    @After
+    public void closeServer() {
+        exService.shutdownNow();
     }
 }
--- src/test/java/ru/ifmo/rain/serdiukov/sd/refactoring/util/AppHTTPClient.java
+++ src/test/java/ru/ifmo/rain/serdiukov/sd/refactoring/util/AppHTTPClient.java
@@ -2,15 +2,24 @@ package ru.ifmo.rain.serdiukov.sd.refactoring.util;
 
 import lombok.NonNull;
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 public class AppHTTPClient implements AutoCloseable {
     private final HttpClient httpClient;
+    private final @NotNull String serverUrl;
 
-    public AppHTTPClient() {
+    public AppHTTPClient(final @NonNull String serverUrl) {
+        this.serverUrl = serverUrl;
         httpClient = new HttpClient();
         try {
             httpClient.start();
@@ -25,12 +34,49 @@ public class AppHTTPClient implements AutoCloseable {
         httpClient.stop();
     }
 
-    final List<Product> getProducts(final @NonNull String server_url) throws APIRequestException {
+    public List<Product> getProducts() throws APIRequestException {
+        final String servletUrl = String.format("%s/get-products", serverUrl);
+        final String responseHTML;
         try {
-            httpClient.GET(String.format("%s/get-products", server_url));
+            final ContentResponse response = httpClient.newRequest(servletUrl)
+                    .timeout(5, TimeUnit.SECONDS)
+                    .send();
+            responseHTML = response.getContentAsString();
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            throw new APIRequestException("Cannot get products", e);
+            throw new APIRequestException("Cannot get products due to the network communication issues", e);
         }
-        throw new UnsupportedOperationException("TODO: Not yet implemented");
+
+        final String[] lines = responseHTML.split("\r\n");
+        final int lineCount = lines.length;
+        final List<Optional<Product>> optionalProducts = Arrays.stream(lines).skip(1).limit(lineCount - 2).map(s -> {
+            // Let's help Java infer types:
+            final Optional<Product> result;
+            final String[] parts = s.split("\\s");
+            if (parts.length != 2) {
+                result = Optional.empty();
+                return result;
+            }
+            final String name = parts[0];
+            final String priceString = parts[1].replaceAll("</br>", "");
+            final long price;
+            try {
+                price = Long.parseLong(priceString);
+            } catch (final NumberFormatException ignored) {
+                result = Optional.empty();
+                return result;
+            }
+            result = Optional.of(new Product(name, price));
+            return result;
+        }).collect(Collectors.toList());
+
+        final List<Product> products = new ArrayList<>();
+        for (final Optional<Product> o : optionalProducts) {
+            if (!o.isPresent()) {
+                throw new APIRequestException("Malformed product record presents in server response");
+            }
+            products.add(o.get());
+        }
+
+        return products;
     }
 }
