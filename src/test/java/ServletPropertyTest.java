1a205e1b83d9975e2a8a5ed9e30d031013893719
Alexander Serdyukov <ntwwwnt@gmail.com>
2021-10-09 17:29:35 +0300

    Test dependencies added

---
+++ src/test/java/ServletPropertyTest.java
@@ -0,0 +1,12 @@
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitQuickcheck.class)
+public class ServletPropertyTest {
+
+    @Property(trials = 500)
+    public void test(){
+
+    }
+}
---
+++ src/test/java/ServletUnitTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class ServletUnitTest {
+    @Test
+    public void someTest(){
+        // Added to check compilation warnings
+    }
+}
