c6ac45331b79a2dd3a8ff9c7b716fe0dca22f9d0
Alexander <ntwwwnt@gmail.com>
2021-10-10 13:23:22 +0300

    Added better synchronization primitives for the server starter.

--- src/test/java/ru/ifmo/rain/serdiukov/sd/refactoring/util/SingletonServerStarter.java
+++ src/test/java/ru/ifmo/rain/serdiukov/sd/refactoring/util/SingletonServerStarter.java
@@ -2,6 +2,7 @@ package ru.ifmo.rain.serdiukov.sd.refactoring.util;
 
 import ru.akirakozov.sd.refactoring.Main;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,12 +12,14 @@ public class SingletonServerStarter {
 
     static {
         System.out.println("Starting up server");
+        final CountDownLatch serverThreadReadiness = new CountDownLatch(1);
         exService.execute(new Runnable() {
             @Override
             public void run() {
                 System.out.println("Server thread started");
                 System.out.flush();
                 try {
+                    serverThreadReadiness.countDown();
                     Main.main(new String[0]);
                 } catch (final InterruptedException ignored) {
                     // That's ok, we are shutting down server after tests
@@ -28,13 +31,14 @@ public class SingletonServerStarter {
             }
         });
         try {
+            // Wait for the server thread to start:
+            serverThreadReadiness.await();
             // Take a break to complete jetty-servlet bootstrap process (as it is a 'black-box' at the current stage):
-            Thread.sleep(5000);
+            Thread.sleep(1500);
         } catch (final InterruptedException ignored) {
-            // Ok
+            // Ok, we are shutting down
         }
     }
-
 
     public static AppHTTPClient getAppClient() {
         return appClient;
